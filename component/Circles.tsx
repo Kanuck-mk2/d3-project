9cbcaeeb9db807dab2ed3724ab3d6bab73ea1b9a
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-08-07 20:42:30 -0600

    created a circles component

---
+++ component/Circles.tsx
@@ -0,0 +1,23 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import * as d3 from 'd3';
+
+const Circle = () => {
+  const [dataset, setDataset] = useState(generateDataset());
+  const ref = useRef();
+  useEffect(() => {
+    const svgElement = d3.select(ref.current);
+    svgElement
+      .selectAll('circle')
+      .data(dataset)
+      .join('circle')
+      .attr('cx', (d) => d[0])
+      .attr('cy', (d) => d[1])
+      .attr('r', 3);
+  }, [dataset]);
+  useInterval(() => {
+    const newDataset = generateDataset();
+    setDataset(newDataset);
+  }, 2000);
+  return <svg viewBox="0 0 100 50" ref={ref} />;
+};
