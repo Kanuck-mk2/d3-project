86098791318c6804356d68b57424174e0cf60ec6
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-08-14 20:04:22 -0600

    Test.tsx update

--- component/Test.tsx
+++ component/Test.tsx
@@ -1,11 +1,25 @@
-'use client'
+'use client';
 
-import * as d3 from 'd3'
+import * as d3 from 'd3';
+
+import { useState } from 'react';
+
+const width = 600;
+const height = 300;
+const circleRadius = 30;
+const initalMousePosition = { x: width / 2, y: height / 2 };
 
 
-const test = () => {
 
-    
-
-
-}
+const Test = () => {
+  const [mousePosition, setMousePosition] = useState(initalMousePosition);
+  const handleMousemOve = (event) => {
+    const { clientX, clientY } = event;
+    console.log({ clientX, clientY });
+  };
+  return (
+    <svg width={width} height={height} onMouseMove={handleMousemOve}>
+      <circle cx={mousePosition.x} cy={mousePosition.y} r={circleRadius} />
+    </svg>
+  );
+};
